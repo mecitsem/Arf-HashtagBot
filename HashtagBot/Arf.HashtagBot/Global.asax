0649122a2483181b1c982b32ff7b9e93d62a7fbc
Mecit Semerci <mecit.semerci@gmail.com>
2016-07-11 13:37:36 +0300

    Base layers're added

---
+++ HashtagBot/Arf.HashtagBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arf.HashtagBot.WebApiApplication" Language="C#" %>
---
+++ HashtagBot/Arf.HashtagBot/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Arf.HashtagBot
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
